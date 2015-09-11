2e12780c2af9baf215472e67841adf7562c70adb
SH <lhdangerous@gmail.com>
2015-09-11 12:24:50 +0900

    - khanning 의 코드로 아두이노 파일 변경. - khanning의 코드와 시리얼 전송 프로토콜 맞춤작업. (시리얼 전송시 1개 바이트만 전송되어 상위바이트 잘림문제)

--- scratch2LittleBits/scratch2LittleBits/arduino/Scratch2LittleBits_kr/Scratch2LittleBits_kr.ino
+++ scratch2LittleBits/scratch2LittleBits/arduino/Scratch2LittleBits_kr/Scratch2LittleBits_kr.ino
@@ -4,28 +4,35 @@
  *  https://github.com/lhdangerous/scratch2LittleBits_kr으로 프로젝트에 기여/수정요청 해주세요
  *  July 13, 2015
  *  
+ *  시리얼 통신을 통해 값 전달시 상위바이트가 잘리고 하위바이트만 전달되는 오류 수정.
+ *  경기교육지원청 김슬기 선생님이 도와주셨습니다.
+ *  Sep. 10, 2015
+ *  
  */
 
+// Status codes sent from Scratch
 const int READ_PINS = 1;
 const int WRITE_ANALOG = 2;
 const int WRITE_DIGITAL = 3;
 
 // Analog input smoothing
 // http://arduino.cc/en/Tutorial/Smoothing
+const int NUM_READINGS = 10;
+int index = 0;
+int readingsA1[NUM_READINGS];
+int readingsA0[NUM_READINGS];
+int averageA1 = 0;
+int averageA0 = 0;
+int totalA1 = 0;
+int totalA0 = 0;
 
-const int sensorChannels = 20;
-const int maxNumReadings = 3;
-
-int smoothingValues[sensorChannels][maxNumReadings];
-int smoothingIndex[sensorChannels];
-int smoothingTotal[sensorChannels];
-
-int analoglizedD0 = 0; // 딪지털인풋을 안날록극값을로출력학기윟해... high>>255 로출력...
-int averageA1;
-int averageA0;
-
-
-int mlUltValor[3];
+// Reading from Serial
+// http://arduino.cc/en/Serial/read
+const int NUM_OUTPUT_PINS = 3;
+int incomingByte = 0;
+int inputPins[NUM_OUTPUT_PINS];
+int outputPin;
+int outputVal;
 
 void setup() {
   
@@ -36,109 +43,75 @@ void setup() {
   pinMode(1, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(9, OUTPUT);
-
-  mlUltValor[0]=-1;
-  mlUltValor[1]=-1;
-  mlUltValor[2]=-1;
   
-  setupSmoothing();
+  // Initialize the readings array with 0's
+  for (int i = 0; i < NUM_READINGS; i++) {
+    readingsA1[i] = 0;
+    readingsA0[1] = 0;
+  }
 
-}
-
-void setupSmoothing() {
-     averageA1 = 0;
-     averageA0 = 0;
-  
-     for(int i = 0; i < sensorChannels; i++) {
-       for(int j = 0 ; j < maxNumReadings ; j++) {
-         smoothingValues[i][j]=0;
-       }
-       smoothingTotal[i]=0;
-       smoothingIndex[i]=0;
-     }
-     
 }
 
 void loop() {
-  int incomingByte = 0;
   
   // Check if there are bytes on the Serial port
   if (Serial.available() > 0) {
     
-      // Get first available byte
-      incomingByte = Serial.read();
+    // Get first available byte
+    incomingByte = Serial.read();
+    
+    if (incomingByte == READ_PINS) {
+    
+      // Read digital pin 0
+      inputPins[0] = digitalRead(0);
       
-      if (incomingByte == READ_PINS) {
+      // Get averages for analog pins 0 and 1
+      inputPins[1] = averageA0;
+      inputPins[2] = averageA1;
       
-        // Read digital pin 0
-        Serial.write(analoglizedD0);
-        
-        // Get averages for analog pins 0 and 1
-        Serial.write(averageA0);
-        Serial.write(averageA1);
-        
-      } else if (incomingByte == WRITE_ANALOG) {
+      // Send value 
+      for (int i = 0; i < NUM_OUTPUT_PINS; i++)
+        Serial.write(inputPins[i]/4);               //   Serial.write() 는 1 byte 만 전송함믈로  max 값이1방잍틀로푷현간능한255 가됭엉야함
       
-        // Next byte from Scratch is pin number
-        int outputPin = Serial.read();
-        
-        // Next byte from Scratch is pin value
-        int outputVal = Serial.read();
-        
-        if(outputPin==5 && mlUltValor[1]!=outputVal){
-            mlUltValor[1]=outputVal;
-            analogWrite(outputPin, outputVal);
-        }
-        if(outputPin==9 && mlUltValor[2]!=outputVal){
-            mlUltValor[2]=outputVal;
-            analogWrite(outputPin, outputVal);
-        }
-      } else if (incomingByte == WRITE_DIGITAL) {
+    
+    } else if (incomingByte == WRITE_ANALOG) {
+    
+      // Next byte from Scratch is pin number
+      outputPin = Serial.read();
       
-        // Next byte from Scratch is pin number
-        int outputPin = Serial.read();
-        
-        // Next byte from Scratch is pin value
-        int outputVal = Serial.read();
-        
-        if(mlUltValor[0]!=outputVal){
-           mlUltValor[0]=outputVal;
-           digitalWrite(outputPin, outputVal);
-        }
+      // Next byte from Scratch is pin value
+      outputVal = Serial.read();
       
+      analogWrite(outputPin, outputVal);
+    
+    } else if (incomingByte == WRITE_DIGITAL) {
+    
+      // Next byte from Scratch is pin number
+      outputPin = Serial.read();
+      
+      // Next byte from Scratch is pin value
+      outputVal = Serial.read();
+      
+      digitalWrite(outputPin, outputVal);
+    
     }
+    
   }
-  //  딪짙턾 d0 핀값이0임면0, 1임면255 
-   analoglizedD0 = digitalRead(0)? 255: 0;
-   
+  
   // Analog input smoothing
   // http://arduino.cc/en/Tutorial/Smoothing
-  averageA0 = readAnalogPort(A0);
-  averageA1 = readAnalogPort(A1);
+  totalA0 = totalA0 - readingsA0[index];
+  totalA1 = totalA1 - readingsA1[index];
+  readingsA0[index] = analogRead(A0);
+  readingsA1[index] = analogRead(A1);
+  totalA0 = totalA0 + readingsA0[index];
+  totalA1 = totalA1 + readingsA1[index];
+  averageA0 = totalA0 / NUM_READINGS;
+  averageA1 = totalA1 / NUM_READINGS;
+  index = index + 1;
+  if (index >= NUM_READINGS)
+    index = 0;
   
-  delay(16);
+  // Slight delay between loop
+  delay(1);
 }
-
-
-int smoothingValue(int channel, int value) {
-    int total;
-    int index = smoothingIndex[channel];
-    total = smoothingTotal[channel] - smoothingValues[channel][index];
-    smoothingValues[channel][index] = value;
-    smoothingTotal[channel] = total + value;
-    smoothingIndex[channel]++;
-    if(smoothingIndex[channel] >=maxNumReadings) {
-      smoothingIndex[channel]=0;
-    }
-
-    return int(round((double)smoothingTotal[channel] / ((double)maxNumReadings)));
-}
-
-int readAnalogPort(int plPin) {
-  int value;
-  value = analogRead(plPin);
-  value = smoothingValue(plPin, value);
-  if (value >= 1020) value = 1020;//pq la division por 4 no debe de superar 255
-  return ((double)value)/4.0;
-}
-
